1ed00faa52db05b8b6c91019fca8d2171800dd08
Eecodev <mnasser.bbk@gmail.com>
2023-11-29 15:06:13 +0100

    initial scaffolding and data printed on page

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 
 <?php
 include __DIR__ ."/partials/header.php";
+// var_dump($hotels);
+
+
 
 ?>
 
     <main class="container">
-
+        <div>
+            <?php foreach($hotels as $hotel){ ?>
+                <ul>
+                    <li><?php echo $hotel['name']?></li>
+                    <li><?php echo $hotel['description']?></li>
+                    <li><?php echo $hotel['parking']?></li>
+                    <li><?php echo $hotel['vote']?></li>
+                    <li><?php echo $hotel['distance_to_center']?></li>
+                </ul>
+            <?php } ?>
+        </div>
     </main>
