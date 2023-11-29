e9d07bb094db0fb13a4272ac3a6bfc44585deb94
Eecodev <mnasser.bbk@gmail.com>
2023-11-29 14:52:06 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+include __DIR__ ."/partials/header.php";
+
+?>
+
+    <main class="container">
+
+    </main>
---
+++ partials/header.php
@@ -0,0 +1,19 @@
+
+<?php
+include __DIR__ ."/../model/db.php";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php hotel</title>
+</head>
+<body>
+
+    <header class="container">
+        
+    </header>
